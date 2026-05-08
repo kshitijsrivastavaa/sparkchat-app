b835045a7b38dbb20445601b20080d5639d93b70
Kshitij Srivastava <kshitij.srivastava16@gmail.com>
2026-05-08 17:23:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true
   }
